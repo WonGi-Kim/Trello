c682033f1d22dee97f16685a41279803e72c0cf9
hyeeunKim <khu4237@gmail.com>
2024-07-10 17:51:18 +0900

    [#2] ✨ feat : Exception 글로벌 처리 기능 추가
    Exception을 글로벌로 처리할 수 있는 기능 추가

---
+++ src/main/java/sparta/trello/global/exception/ErrorCode.java
@@ -0,0 +1,27 @@
+package sparta.trello.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // basic
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD REQUEST"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "FORBIDDEN"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "NOT FOUND"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR"),
+    // 필요시 직접 추가 (위 참고 해서)
+
+    // Token
+    NOT_FOUND_TOKEN(HttpStatus.NOT_FOUND, "토큰이 없습니다."),
+    TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "토큰이 만료되었습니다."),
+    TOKEN_INVALID(HttpStatus.BAD_REQUEST, "토큰이 유효하지 않습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+}
