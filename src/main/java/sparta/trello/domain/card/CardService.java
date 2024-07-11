aea5d7897cabde69a63ed7883d4a2a87f29211b4
이세비 <lsy205105@naver.com>
2024-07-11 10:50:26 +0900

    [#14] 🩹 fix : sequence 수정

--- src/main/java/sparta/trello/domain/card/CardService.java
+++ src/main/java/sparta/trello/domain/card/CardService.java
@@ -37,7 +37,7 @@ public class CardService {
         );
 
         int size = cardRepository.findAll().size();
-        int seq = size * 1024;
+        int seq = (size+1) * 1024;
 
         Card card = Card.builder()
                 .status(status)
