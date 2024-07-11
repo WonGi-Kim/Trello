c5731c471bbf0eb7dcf961decc077bd53507e557
이세비 <lsy205105@naver.com>
2024-07-11 15:54:00 +0900

    [#46] 🩹 fix : Card 생성 유저 부분 변경

--- src/main/java/sparta/trello/domain/card/CardController.java
+++ src/main/java/sparta/trello/domain/card/CardController.java
@@ -4,11 +4,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sparta.trello.domain.card.dto.CardRequestDto;
 import sparta.trello.domain.card.dto.CardResponseDto;
 import sparta.trello.domain.card.dto.NicknameRequestDto;
+import sparta.trello.domain.user.User;
 import sparta.trello.global.common.CommonResponse;
+import sparta.trello.global.security.UserPrincipal;
 
 import java.util.List;
 
@@ -18,10 +21,10 @@ public class CardController {
     private final CardService cardService;
 
     @PostMapping("/boards/{boardId}/status/{statusId}/cards")
-    public ResponseEntity<CommonResponse<CardResponseDto>> createCard(@Valid @RequestBody CardRequestDto requestDto,
-                                                                      @PathVariable Long statusId, @PathVariable Long boardId) {
-
-        CardResponseDto responseDto = cardService.create(requestDto, statusId, boardId);
+    public ResponseEntity<CommonResponse<CardResponseDto>> createCard
+            (@AuthenticationPrincipal UserPrincipal principal, @Valid @RequestBody CardRequestDto requestDto, @PathVariable Long statusId, @PathVariable Long boardId) {
+        User user = principal.getUser();
+        CardResponseDto responseDto = cardService.create(requestDto, statusId, boardId, user);
         return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResponse<>("카드 생성 성공", HttpStatus.CREATED.value(), responseDto));
     }
 
