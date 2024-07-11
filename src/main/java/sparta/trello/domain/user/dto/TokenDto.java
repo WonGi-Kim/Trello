8c9659d28099700f9e163d6e8c2f1da940bb40e8
heKim <khu4237@gmail.com>
2024-07-11 20:03:34 +0900

    [#58] ✨ feat : token 재발급 기능 추가
    token 재발급 기능 추가

--- src/main/java/sparta/trello/domain/user/dto/TokenDto.java
+++ src/main/java/sparta/trello/domain/user/dto/TokenDto.java
@@ -3,14 +3,16 @@ package sparta.trello.domain.user.dto;
 import lombok.Data;
 
 @Data
-public class LoginResponseDto {
+public class TokenDto {
 
     private String access;
     private String refresh;
 
-    public LoginResponseDto(String access, String refresh) {
+    public TokenDto(String access, String refresh) {
+
         this.access = access;
         this.refresh = refresh;
+
     }
 
 }
