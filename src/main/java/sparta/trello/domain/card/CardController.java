59fbe6fdf621091ef24fbadc4ce646d90295462e
이세비 <lsy205105@naver.com>
2024-07-11 10:50:41 +0900

    [#14] 🩹 fix : 경로 및 변수 수정

--- src/main/java/sparta/trello/domain/card/CardController.java
+++ src/main/java/sparta/trello/domain/card/CardController.java
@@ -8,16 +8,15 @@ import org.springframework.web.bind.annotation.*;
 import sparta.trello.global.common.CommonResponse;
 
 @RestController
-@RequestMapping("/cards")
 @RequiredArgsConstructor
 public class CardController {
     private final CardService cardService;
 
     @PostMapping("/boards/{boardId}/status/{statusId}/cards")
     public ResponseEntity<CommonResponse<CardResponseDto>> createCard(@RequestBody CardRequestDto requestDto,
-                                     @PathVariable Long columnId, @PathVariable Long boardId) {
+                                     @PathVariable Long statusId, @PathVariable Long boardId) {
 
-        CardResponseDto responseDto = cardService.create(requestDto, columnId, boardId);
+        CardResponseDto responseDto = cardService.create(requestDto, statusId, boardId);
         return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResponse<>("카드 생성 성공", HttpStatus.CREATED.value(), responseDto));
     }
 }
