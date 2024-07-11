649faa4eb45f23e0b778ae765c6ef7565fb3c535
이세비 <lsy205105@naver.com>
2024-07-11 15:57:34 +0900

    [#46] 🩹 fix : 카드 조회 주석 처리

--- src/main/java/sparta/trello/domain/card/CardController.java
+++ src/main/java/sparta/trello/domain/card/CardController.java
@@ -28,24 +28,24 @@ public class CardController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new CommonResponse<>("카드 생성 성공", HttpStatus.CREATED.value(), responseDto));
     }
 
-    @GetMapping("/boards/{boardId}/cards")
-    public ResponseEntity<CommonResponse<List<CardResponseDto>>> findCardList(@PathVariable Long boardId) {
-        List<CardResponseDto> responseDtos = cardService.findCardList(boardId);
-        return ResponseEntity.status(HttpStatus.OK).body(new CommonResponse<>("전체 목록 조회 성공", HttpStatus.OK.value(), responseDtos));
-    }
-
-    @GetMapping("boards/{boardId}/cards")
-    public ResponseEntity<CommonResponse<List<CardResponseDto>>> findCardListByStatus
-            (@PathVariable Long boardId, @RequestParam("status") Long statusId) {
-        List<CardResponseDto> responseDtos = cardService.findCardListByStatus(boardId, statusId);
-        return ResponseEntity.status(HttpStatus.OK).body(new CommonResponse<>("상태 별 목록 조회 성공", HttpStatus.OK.value(), responseDtos));
-    }
-
-    @GetMapping("/boards/{boardId}/cards")
-    public ResponseEntity<CommonResponse<List<CardResponseDto>>> findCardListByUser(@RequestParam("nickname") String nickname, @PathVariable Long boardId) {
-        List<CardResponseDto> responseDtos = cardService.findCardListByUser(nickname, boardId);
-        return ResponseEntity.status(HttpStatus.OK).body(new CommonResponse<>("작성자 별 목록 조회 성공", HttpStatus.OK.value(), responseDtos));
-    }
+//    @GetMapping("/boards/{boardId}/cards")
+//    public ResponseEntity<CommonResponse<List<CardResponseDto>>> findCardList(@PathVariable Long boardId) {
+//        List<CardResponseDto> responseDtos = cardService.findCardList(boardId);
+//        return ResponseEntity.status(HttpStatus.OK).body(new CommonResponse<>("전체 목록 조회 성공", HttpStatus.OK.value(), responseDtos));
+//    }
+//
+//    @GetMapping("boards/{boardId}/cards")
+//    public ResponseEntity<CommonResponse<List<CardResponseDto>>> findCardListByStatus
+//            (@PathVariable Long boardId, @RequestParam("status") Long statusId) {
+//        List<CardResponseDto> responseDtos = cardService.findCardListByStatus(boardId, statusId);
+//        return ResponseEntity.status(HttpStatus.OK).body(new CommonResponse<>("상태 별 목록 조회 성공", HttpStatus.OK.value(), responseDtos));
+//    }
+//
+//    @GetMapping("/boards/{boardId}/cards")
+//    public ResponseEntity<CommonResponse<List<CardResponseDto>>> findCardListByUser(@RequestParam("nickname") String nickname, @PathVariable Long boardId) {
+//        List<CardResponseDto> responseDtos = cardService.findCardListByUser(nickname, boardId);
+//        return ResponseEntity.status(HttpStatus.OK).body(new CommonResponse<>("작성자 별 목록 조회 성공", HttpStatus.OK.value(), responseDtos));
+//    }
 
 
 }
