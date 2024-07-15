148b2c37750c6413b18a346465798a38e8edd35a
heKim <khu4237@gmail.com>
2024-07-15 10:20:53 +0900

    [#87] 🩹 fix : 에러 메세지 출력 수정
    bean validation 에러 메세지에 전체 에러 내용 나오는 것 수정

--- src/main/java/sparta/trello/global/exception/GlobalExceptionHandler.java
+++ src/main/java/sparta/trello/global/exception/GlobalExceptionHandler.java
@@ -3,12 +3,14 @@ package sparta.trello.global.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sparta.trello.global.common.CommonErrorResponse;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,9 +31,15 @@ public class GlobalExceptionHandler {
     // bean validation error
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CommonErrorResponse> handleException(final MethodArgumentNotValidException exception, final HttpServletRequest request) {
+        String errorMessage = exception.getBindingResult().getAllErrors().stream()
+                .map(error -> {
+                    FieldError fieldError = (FieldError) error;
+                    return String.format("%s %s", fieldError.getField(), fieldError.getDefaultMessage());
+                })
+                .collect(Collectors.joining("\n"));
         return ResponseEntity.status(exception.getStatusCode()).body(
                 CommonErrorResponse.builder()
-                        .msg(exception.getMessage())
+                        .msg(errorMessage)
                         .status(HttpStatus.BAD_REQUEST.value())
                         .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
                         .path(request.getRequestURI())
